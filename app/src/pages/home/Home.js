1cf78e33097bcc1652df01281535dd095868cb9d
Catalin Banici <catalinbanici95@yahoo.com>
2023-07-11 21:50:37 +0200

    home page styled

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,17 +1,51 @@
 import React from "react";
-import HomeCard from "./components/HomeCard";
 import { Link } from "react-router-dom";
 
+import "./Home.css";
+
+import { FiInfo } from "react-icons/fi";
+import { FiBriefcase } from "react-icons/fi";
+import { FiSend } from "react-icons/fi";
+
 export default function Home() {
-  const links = ["about", "projects", "contact"];
   return (
-    <>
-      <h1>Welcome</h1>
-      {links.map((element, index) => (
-        <Link to={element} key={index}>
-          <HomeCard link={element} />
-        </Link>
-      ))}
-    </>
+    <div className="home-container">
+      <div className="home-message-container">
+        <h1>Hello!</h1>
+        <h2>My name is Catalin</h2>
+        <h3>Welcome to my portfolio website!</h3>
+      </div>
+      <div className="home-cards-container">
+        <div className="about-contact-cards">
+          <div className="about-card">
+            <Link to="about">
+              <div>
+                <p>Find out about me and my skills.</p>
+                <FiInfo className="svg" />
+              </div>
+              <h2>About</h2>
+            </Link>
+          </div>
+          <div className="contact-card">
+            <Link to="contact">
+              <div>
+                <p>Do you have any questions? Send me a message!</p>
+                <FiSend className="svg" />
+              </div>
+              <h2>Contact</h2>
+            </Link>
+          </div>
+        </div>
+        <div className="projects-card">
+          <Link to="projects">
+            <div>
+              <p>Check out my work so far.</p>
+              <FiBriefcase className="svg" />
+            </div>
+            <h2>Projects</h2>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
--- app/src/pages/home/components/HomeCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function HomeCard({ link }) {
-  return (
-    <div>
-      <h2>{link}</h2>
-    </div>
-  );
-}
